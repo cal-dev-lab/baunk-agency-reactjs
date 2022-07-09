fa04cb1c5db9b309f3d8725d0fe51454dc1d2317
Callum Lees <hi@c-l.dev>
2022-07-09 23:04:09 +0100

    Child component with retrieved data as props

--- src/index.js
+++ src/index.js
@@ -4,9 +4,15 @@ import './index.css';
 import App from './App';
 import { BrowserRouter as Router } from "react-router-dom";
 
+import { ApolloProvider } from "@apollo/react-hooks";
+
+import client from "./utils/apolloClient";
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <Router>
-    <App />
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
   </Router>
 );
