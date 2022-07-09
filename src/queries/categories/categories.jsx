53c3d67d28fd14160c6e4f948d3cff1d559afae5
Callum Lees <hi@c-l.dev>
2022-07-09 23:04:28 +0100

    Created GQL categories

---
+++ src/queries/categories/categories.jsx
@@ -0,0 +1,17 @@
+import gql from "graphql-tag";
+
+const CATEGORIES_QUERY = gql`
+    
+    query Categories {
+        categories {
+            data {
+                attributes {
+                    slug
+                    name
+                }
+            }
+        }
+    }
+`;
+
+export default CATEGORIES_QUERY;
