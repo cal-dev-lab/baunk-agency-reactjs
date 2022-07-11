9a0b596f20b0620968fb53f67ac1b6d53d49b8c1
Callum Lees <hi@c-l.dev>
2022-07-11 17:39:07 +0100

    Added Baunk and Avenir typeface

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,12 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        avenir: ["AVENIR", "sans-serif"],
+        baunk: ["BAUNK", "sans-serif"],
+      },
+    },
   },
   plugins: [],
-}
+};
