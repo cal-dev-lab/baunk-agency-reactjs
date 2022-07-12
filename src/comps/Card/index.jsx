c9bbe26db1fd1df136d638cbacac1d0822491bc2
Callum Lees <hi@c-l.dev>
2022-07-12 09:02:39 +0100

    Created Card component

---
+++ src/comps/Card/index.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Card = ({ article }) => {
+  const imageUrl =
+    process.env.NODE_ENV !== "development"
+      ? article.attributes.image.data.attributes.url
+      : process.env.REACT_APP_BACKEND_URL +
+        article.attributes.image.data.attributes.url;
+  return (
+    <Link to={`/article/${article.attributes.slug}`}>
+      <div className="group rounded-lg">
+        <div className="w-full rounded-t-lg bg-transparent">
+          <img src={imageUrl} alt={article.attributes.image.url} className="h-64 w-full object-cover" />
+        </div>
+        <div className="py-2">
+          <p id="category" className="text-xl font-avenir font-bold py-2">
+            {article.attributes.category.data.attributes.name}
+          </p>
+          <p id="title" className="text-2xl font-avenir">
+            {article.attributes.title}
+          </p>
+        </div>
+      </div>
+    </Link>
+  );
+};
+
+export default Card;
