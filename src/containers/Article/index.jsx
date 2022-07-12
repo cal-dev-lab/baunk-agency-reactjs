ac0b8d36fb17262d9ece8c1563dfcd161edf89bb
Callum Lees <hi@c-l.dev>
2022-07-12 22:38:55 +0100

    Updated index to 0

--- src/containers/Article/index.jsx
+++ src/containers/Article/index.jsx
@@ -13,15 +13,15 @@ const Article = () => {
         if (articles.data.length) {
           const imageUrl =
             process.env.NODE_ENV !== "development"
-              ? articles.data[5].attributes.image.data.attributes.url
+              ? articles.data[0].attributes.image.data.attributes.url
               : process.env.REACT_APP_BACKEND_URL +
-                articles.data[5].attributes.image.data.attributes.url;
+                articles.data[0].attributes.image.data.attributes.url;
 
 
           return (
             <div>
                 <img src={imageUrl} />
-                <h1>{articles.data[5].attributes.title}</h1>
+                <h1>{articles.data[0].attributes.title}</h1>
             </div>
           );
 
