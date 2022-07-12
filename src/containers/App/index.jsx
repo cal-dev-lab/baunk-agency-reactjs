5371c59b28120a139b91759870ad9a62209c2b78
Callum Lees <hi@c-l.dev>
2022-07-12 21:33:32 +0100

    Imported and used routes

--- src/containers/App/index.jsx
+++ src/containers/App/index.jsx
@@ -1,14 +1,20 @@
 import React from "react";
 import ArticleNav from "../../comps/ArticleNav";
 import Articles from "../../containers/Articles";
+import Article from "../../containers/Article";
 import Nav from "../../comps/Nav";
+
+import { Routes, Route } from "react-router-dom";
 
 const App = () => {
     return (
         <div className="App">
             <Nav />
             <ArticleNav />
-            <Articles />
+            <Routes>
+                <Route path="/" element={<Articles />} exact />
+                <Route path="/article/:slug" element={<Article />} exact />
+            </Routes>
         </div>
     );
 }
