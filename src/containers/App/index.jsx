4e7afa5bad953fc343ed44c855d7c5c7b12bab33
Callum Lees <hi@c-l.dev>
2022-07-12 09:03:03 +0100

    Removed jsx fragments

--- src/containers/App/index.jsx
+++ src/containers/App/index.jsx
@@ -1,11 +1,15 @@
 import React from "react";
+import ArticleNav from "../../comps/ArticleNav";
+import Articles from "../../containers/Articles";
 import Nav from "../../comps/Nav";
 
 const App = () => {
     return (
-        <>
+        <div className="App">
             <Nav />
-        </>
+            <ArticleNav />
+            <Articles />
+        </div>
     );
 }
 
