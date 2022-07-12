39535daa85eaf2e86e86a87a791ed727f272e454
Callum Lees <hi@c-l.dev>
2022-07-12 21:35:41 +0100

    Revert "Imported and used routes"
    This reverts commit 5371c59b28120a139b91759870ad9a62209c2b78.

--- src/containers/App/index.jsx
+++ src/containers/App/index.jsx
@@ -1,20 +1,14 @@
 import React from "react";
 import ArticleNav from "../../comps/ArticleNav";
 import Articles from "../../containers/Articles";
-import Article from "../../containers/Article";
 import Nav from "../../comps/Nav";
-
-import { Routes, Route } from "react-router-dom";
 
 const App = () => {
     return (
         <div className="App">
             <Nav />
             <ArticleNav />
-            <Routes>
-                <Route path="/" element={<Articles />} exact />
-                <Route path="/article/:slug" element={<Article />} exact />
-            </Routes>
+            <Articles />
         </div>
     );
 }
