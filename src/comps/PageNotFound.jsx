ca10560dce60714c07e7429a6d1f7a4cd838c3fa
Callum Lees <hi@c-l.dev>
2022-07-14 15:38:58 +0100

    Added useLocation to output current pathname

--- src/comps/PageNotFound.jsx
+++ src/comps/PageNotFound.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
 import Nav from './Nav';
 import Pnf from '../images/404.svg';
+import { useLocation } from 'react-router-dom';
 
-const PageNotFound = ({ location }) => {
+const PageNotFound = () => {
+
+    let location = useLocation();
+
     return (
         <>
 
@@ -12,12 +16,16 @@ const PageNotFound = ({ location }) => {
 
                 <div className='font-baunk text-4xl text-red-500 text-center'>
 
-                    <img src={Pnf} alt="" />
+                    <img src={Pnf} alt="" className='h-60 w-full' />
 
-                    <h3 className='text-3xl font-avenir'>No match for {location.pathname}</h3>
+                    <h3 className='text-3xl font-avenir pt-20'>
+                        No match for <code>{location.pathname}</code>
+                    </h3>
 
                     <a href='/'>
-                        <p className='font-avenir text-xl mt-10 text-red-500 underline'>&larr; Let's take you back home</p>
+                        <p className='font-avenir text-xl mt-10 text-red-500 underline'>
+                            &larr; Let's take you back home
+                        </p>
                     </a>
 
                 </div>
