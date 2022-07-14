d0d201c3be664d2bfa88e41790c46c4a0c380c7c
Callum Lees <hi@c-l.dev>
2022-07-14 17:18:38 +0100

    Added Link to PNF

--- src/comps/PageNotFound.jsx
+++ src/comps/PageNotFound.jsx
@@ -1,32 +1,31 @@
 import React from 'react'
-import Nav from './Nav';
 import Pnf from '../images/404.svg';
-import { useLocation } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 
 const PageNotFound = () => {
 
+    // Set location varible
     let location = useLocation();
+
+    // Check it is finding correct page
+    console.log(`No route found for: ${location.pathname}`);
 
     return (
         <>
-
-        <Nav />
 
             <div className='max-w-7xl mx-auto py-2 min-h-screen flex items-center justify-center'>
 
                 <div className='font-baunk text-4xl text-red-500 text-center'>
 
-                    <img src={Pnf} alt="" className='h-60 w-full' />
+                    <img src={Pnf} alt="" className='h-60 w-full -mt-20' />
 
-                    <h3 className='text-3xl font-avenir pt-20'>
-                        No match for <code>{location.pathname}</code>
-                    </h3>
+                    <h3 className='font-avenir mt-16'>No page found for: <code className='p-2 bg-red-50 rounded-lg'>{location.pathname}</code></h3>
 
-                    <a href='/'>
+                    <Link to='/'>
                         <p className='font-avenir text-xl mt-10 text-red-500 underline'>
                             &larr; Let's take you back home
                         </p>
-                    </a>
+                    </Link>
 
                 </div>
 
