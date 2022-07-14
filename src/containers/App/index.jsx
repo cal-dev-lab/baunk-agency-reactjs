2ffd76f7f035d56a6c89b91285c35daab98a8868
Callum Lees <hi@c-l.dev>
2022-07-14 14:04:04 +0100

    Added PageNotFound component

--- src/containers/App/index.jsx
+++ src/containers/App/index.jsx
@@ -1,11 +1,15 @@
 import React from "react";
+
+import Nav from "../../comps/Nav";
 import ArticleNav from "../../comps/ArticleNav";
 import Articles from "../../containers/Articles";
 import Article from "../../containers/Article";
 import Category from "../../containers/Category";
-import Nav from "../../comps/Nav";
 
-import { Routes, Route } from "react-router-dom";
+import PageNotFound from "../../comps/PageNotFound";
+
+import {Routes, Route } from "react-router-dom";
+
 
 const App = () => {
     return (
@@ -16,6 +20,7 @@ const App = () => {
                 <Route path="/" element={<Articles />} exact />
                 <Route path="/article/:slug" element={<Article />} exact />
                 <Route path="/category/:slug" element={<Category />} exact />
+                <Route component={PageNotFound} />
             </Routes>
         </div>
     );
