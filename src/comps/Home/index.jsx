045c6f604cd3edbb3223e1512fdd5b4ea8e92770
Callum Lees <hi@c-l.dev>
2022-07-14 17:18:10 +0100

    Create Home component

---
+++ src/comps/Home/index.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import ArticleNav from '../ArticleNav';
+import Blog from '../Blog';
+
+const Home = () => {
+  return (
+    <>
+        <ArticleNav />
+        <Blog />
+    </>
+  )
+}
+
+export default Home;
